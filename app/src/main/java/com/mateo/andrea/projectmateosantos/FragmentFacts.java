d17e86bdbf505fcace5fa870fcfbbdfc1916cb2d
andreamateo <andreafaith.mateo.iics@ust.edu.ph>
2018-11-12 14:56:11 +0800

    Initial commit

---
+++ app/src/main/java/com/mateo/andrea/projectmateosantos/FragmentFacts.java
@@ -0,0 +1,22 @@
+package com.mateo.andrea.projectmateosantos;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFacts extends Fragment {
+    View view;
+    public FragmentFacts() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view =inflater.inflate(R.layout.facts_fragment,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/mateo/andrea/projectmateosantos/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mateo.andrea.projectmateosantos;
+
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tablayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tablayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new FragmentFacts(), "Facts");
+        adapter.AddFragment(new FragmentCheck(), "Check");
+        adapter.AddFragment(new FragmentLogs(), "Logs");
+
+        viewPager.setAdapter(adapter);
+        tablayout.setupWithViewPager(viewPager);
+
+    }
+}

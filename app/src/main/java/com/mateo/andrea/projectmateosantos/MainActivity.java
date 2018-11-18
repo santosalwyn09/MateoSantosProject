b2a73e7dbb8b03d285687f0128c268a76bb47024
santosalwyn09 <alwynnico.santos.iics@ust.edu.ph>
2018-11-18 17:44:45 +0800

    UI Commit v2

--- app/src/main/java/com/mateo/andrea/projectmateosantos/MainActivity.java
+++ app/src/main/java/com/mateo/andrea/projectmateosantos/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mateo.andrea.projectmateosantos;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,15 +9,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 4000;
     private TabLayout tablayout;
     private AppBarLayout appBarLayout;
     private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         tablayout = (TabLayout) findViewById(R.id.tablayout_id);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);
